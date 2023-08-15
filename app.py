189ac92eb6bb4d938b6836eb35b5ac8ef4b7d39f
Ashok singh <rwtashok2000@gmail.com>
2023-08-15 09:07:31 +0530

    modified app,helper

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import preprocessor
+import preprocessor, helper
+import matplotlib.pyplot as plt
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
@@ -37,3 +38,18 @@ if uploaded_file is not None:
         with col4:
             st.header("Links Shared")
             st.title(num_links)
+
+            # finding the busiest users in the group(Group level)
+            if selected_user == 'Overall':
+                st.title('Most Busy Users')
+                x, new_df = helper.most_busy_users(df)
+                fig, ax = plt.subplots()
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    ax.bar(x.index, x.values, color='red')
+                    plt.xticks(rotation='vertical')
+                    st.pyplot(fig)
+                with col2:
+                    st.dataframe(new_df)
